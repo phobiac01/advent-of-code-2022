3c2e17e4cc3f5ceba3695ec3c9e5ba766024a7d2
phobiac01 <phobiac01@gmail.com>
2022-12-07 21:51:17 -0700

    Init

---
+++ pls_select.js
@@ -0,0 +1,6 @@
+console.log(
+    " =====================================================\n", 
+    "Whoops! It looks like you forgot to select a solution file to run!\n",
+    "Please select a file from the 'solutions' directory, like this:  node solutions/day1.js\n",
+    "=====================================================\n"
+);
