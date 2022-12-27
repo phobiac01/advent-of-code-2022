4235049ff33767fed961aaf31a498eb6e9f06b83
phobiac01 <phobiac01@gmail.com>
2022-12-27 15:59:47 -0700

    Completed Day 5 - parts 1 & 2

--- solutions/day5.js
+++ solutions/day5.js
@@ -27,6 +27,39 @@ var allStacks = [
     "GNFH",
 ];
 
-console.log(allStacks);
+const solutionLogic = "part2";
 
-// allStacks[1] = [ "" ];
+// Apply each step to the allStacks cache [ number, from-stack, to-stack]
+parsedInstructions.forEach(instruction => {
+    var number = instruction[0];
+    var fromStack = instruction[1] - 1;
+    var toStack = instruction[2] - 1;
+    var targetCrates = allStacks[fromStack].substring(allStacks[fromStack].length - number);
+
+    console.log(instruction, "Before:", allStacks[fromStack], "|", allStacks[toStack]);
+
+    // Remove the selected crates off the top of the fromStack
+    allStacks[fromStack] = allStacks[fromStack].substring(0, allStacks[fromStack].length - number);
+
+    if (solutionLogic == "part1") {
+        // Add each selected crate to the new stack, processing from top to bottom
+        targetCrates = targetCrates.split("").reverse();
+        targetCrates.forEach(crate => allStacks[toStack] += crate);
+    } else {
+        // Add all of the selected crates to the new stack, preserving their order
+        targetCrates = targetCrates.split("");
+        targetCrates.forEach(crate => allStacks[toStack] += crate);
+    }
+
+    console.log(instruction, "After:", allStacks[fromStack], "|", allStacks[toStack]);
+    console.log("-");
+});
+
+console.log(`====== SL: ${solutionLogic} ======\n`, allStacks);
+
+var topCrates = "";
+allStacks.forEach(stack => {
+    topCrates += stack[stack.length - 1];
+});
+
+console.log({topCrates});
